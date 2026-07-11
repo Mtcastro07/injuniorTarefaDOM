0704ed4faf6f4f86538576973e3dcb9c7282fa79
Matheus <matheus.oliveira.luna.garcia@gmail.com>
2026-07-11 12:33:37 -0300

    feat: Lista editavel adicionada, faltando apenas sua estilização

--- script.js
+++ script.js
@@ -12,31 +12,56 @@ let listaProdutos = document.querySelector(".lista");
 let lista = document.querySelector(".fundoLista");
 let edicao = document.querySelector(".editar");
 let menuEdicao = document.querySelector(".fundoEdicoes");
-produtos = [];
+let listaEditavel = document.querySelector(".list-edit");
+let concluir = document.querySelector(".concluir");
+
+let produtos = [];
 
 function criarLista(products) {
-  listaProdutos.classList.add("lista");
-  let li = document.createElement("li");
-  let divisao = document.createElement("div");
-  li.classList.add("linha");
-  divisao.classList.add("linhas");
-  let name = document.createElement("p");
-  let qntd = document.createElement("p");
-  qntd.classList.add("quantidadeProduto");
-  name.classList.add("nomeProduto");
-  qntd.textContent = products.numero;
-  name.textContent = products.nome;
-  li.append(name, qntd);
-  divisao.appendChild(li);
-  listaProdutos.appendChild(divisao);
-  addItem.classList.remove("aberto");
-  produto.value = "";
-  quantidade.value = "";
+  listaProdutos.innerHTML = "";
+
+  products.forEach((product) => {
+    listaProdutos.classList.add("lista");
+    let li = document.createElement("li");
+    let divisao = document.createElement("div");
+    li.classList.add("linha");
+    divisao.classList.add("linhas");
+    let name = document.createElement("p");
+    let qntd = document.createElement("p");
+    qntd.classList.add("quantidadeProduto");
+    name.classList.add("nomeProduto");
+    qntd.textContent = product.numero;
+    name.textContent = product.nome;
+    li.append(name, qntd);
+    divisao.appendChild(li);
+    listaProdutos.appendChild(divisao);
+    addItem.classList.remove("aberto");
+    produto.value = "";
+    quantidade.value = "";
+  });
   qntdItens.innerText = produtos.length;
   listExibition();
 }
 
-function criarListaEditavel() {}
+function criarListaEditavel(produtos) {
+  listaEditavel.innerHTML = "";
+
+  produtos.forEach((product, indice) => {
+    let li = document.createElement("li");
+    li.classList.add("line-edit");
+    let botaoEditavel = document.createElement("button");
+    botaoEditavel.classList.add("btn-edit");
+    botaoEditavel.innerText = "X";
+
+    li.append(product.nome, product.numero, botaoEditavel);
+    botaoEditavel.addEventListener("click", () => {
+      produtos.splice(indice, 1);
+      criarListaEditavel(produtos);
+      criarLista(produtos);
+    });
+    listaEditavel.appendChild(li);
+  });
+}
 
 function listExibition() {
   if (produtos.length > 0) {
@@ -87,12 +112,14 @@ adicionar.addEventListener("click", () => {
   };
   produtos.push(item);
 
-  criarLista(item);
+  criarLista(produtos);
 });
 
 edicao.addEventListener("click", () => {
-  if (produtos.length == 0) {
-    alert("não há produtos na lista!");
-  } else {
-  }
+  menuEdicao.classList.remove("fechado");
+  criarListaEditavel(produtos);
+});
+
+concluir.addEventListener("click", () => {
+  menuEdicao.classList.add("fechado");
 });
